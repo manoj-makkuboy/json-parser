b33bbadc404797bd3c31b725282f7d53b72f7d2d
Manoj Mohan <m.manoj@linuxmail.org>
2017-05-22 16:33:16 +0530

    added support to handle spaces in JSON without affecting spaces inside keys

--- json_parser.py
+++ json_parser.py
@@ -1,5 +1,5 @@
 import re
-
+import pdb
 
 def string_parser(string):
 
@@ -10,8 +10,8 @@ def string_parser(string):
 
         x = 1
         while (string[x] != '"'):
-            if(string[x] == '\\'):
-                x +=1                   # incrementing pointer when escape sequence is encountered
+            if(string[x] == '\\'):  # encounter of escape sequence
+                x +=1
             parsed_string = parsed_string + string[x]
             x += 1
         unparsed_string = string[x+1:]
@@ -25,40 +25,46 @@ def array_parser(string):
 
 def object_parser(string):
     if(string[0] == '{'):
-
         parsed_dict = {}
         string = string[1:]
+
+        string = string.strip()
 
         while(string[0] != '}'):
 
             if (string[0] == '"'):   # assuming key will be a string
                 key_string_tuple = string_parser(string[:])
                 key, string = key_string_tuple   # return of string_parser of form ("","") tuple
-
-            if (string[0] != ':'):
-                raise SyntaxError(" : not found after key")
-            elif(string[0] == ':'):
+                string = string.strip()    # strip after key creation
+#            if (string[0] != ':'):
+#                raise SyntaxError(" : not found after key")
+            if(string[0] == ':'):
                 string = string[1:]
+#                pdb.set_trace()
+                string = string.strip()
+
 
             if (string[0] == '"'):
                 value_string_tuple = string_parser(string[:])
                 value, string = value_string_tuple
+                string = string.strip()    # strip after value creation
 
             if (string[0] == '{'):
                 value_dict_tuple = object_parser(string)   #recursive call when value is a dict
                 value, string = value_dict_tuple
 
+            string = string.strip()
+#            pdb.set_trace()
             parsed_dict[key] = value   # key: value pair generated
+
 
             if (string[0] == ','):
                 string = string[1:]
                 if(string[0] == '}'):
                     raise SyntaxError(" no key:value pair found after , ")
+
             continue
 
-#        string = string[1:]             # case to find the end of dict in string
-#        if(string == ''):
-#            return (parsed_dict, string)    # base case return
 
         return (parsed_dict, string[1:])
 
@@ -66,7 +72,7 @@ if __name__ == "__main__":
     with open('first.json') as f:
         content = f.read()
 
-    content = ''.join(x.strip(' ').strip('\n').strip('\t') for x in content)
+    content = ''.join(x.strip('\n').strip('\t') for x in content)
     print (content)
     content = re.search(r'(".*?")', content)
     print (content)
