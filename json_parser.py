efd635c1f767bbcd6017d7dff3725847230e2169
Manoj Mohan <m.manoj@linuxmail.org>
2017-05-24 18:00:24 +0530

    added Number parser (review needed for regex) and integrated it with array and object parsers

--- json_parser.py
+++ json_parser.py
@@ -1,44 +1,113 @@
 import pdb
+import re
 
 def string_parser(string):
 
     parsed_string = ''
     unparsed_string = ''
 
+    if(string[0] != '"'):
+        return (None,string.strip())
+
     if(string[0] == '"'):
 
         x = 1
         while (string[x] != '"'):
             if(string[x] == '\\'):  # encounter of escape sequence
-                x +=1
+                x += 1
             parsed_string = parsed_string + string[x]
             x += 1
         unparsed_string = string[x+1:]
+    return (parsed_string, unparsed_string.strip())
 
-    return (parsed_string, unparsed_string)
+
+def value_parser(string):
+    original_string = string
+    parsed_value = None
+
+    if (re.match('true',string) != None):
+        parsed_value = True
+        string = string[4:].strip()
+
+    elif (re.match('false',string) != None):
+        parsed_value = False
+        string = string[5:].strip()
+
+    elif (re.match('null',string) != None):
+        parsed_value = None
+        string =  string[4:].strip()
+
+    if(original_string != string):
+        return (parsed_value, string )
+    else:
+        return None
+
+
+def number_parser(string):
+
+    to_parse_number_str = ''.join(re.findall(r'^((?:-?\d+)(?:\.?\d+)?(?:[Ee][+-]\d+)?)',string))
+    if (len(to_parse_number_str) == 0):
+        return (None, string)
+    try:
+        parsed_number = int(to_parse_number_str)
+    except ValueError:
+        parsed_number = float(to_parse_number_str)
+
+    return (parsed_number, string[len(to_parse_number_str):].strip())
+
 
 
 def array_parser(string):
+    if (string[0] != '['):
+        return (None, string.strip())
     if(string[0] == '['):
         parsed_list = []
         string = string[1:]
 
         while (string[0] != ']'):
 
-            if(string[0] == '"'):
-                value,string = string_parser(string)
+            value,string = string_parser(string)   # checking if the value is string
+
+            if(value != None):
                 parsed_list.append(value)
-                string = string.strip()
-            if(string[0] == '{'):
-                value,string = object_parser(string)
+
+            value,string = object_parser(string) #sending to Object parser
+
+            if(value != None):
                 parsed_list.append(value)
-                string = string.strip()
-            if(string[0] == ','):
-                string = string[1:]
-                continue
-        return (parsed_list, string[1:])
+
+            value,string = number_parser(string) # sending to number_parser
+
+            if (value != None):
+                parsed_list.append(value)
+
+            if(value_parser(string) != None): # if return None means the value is not null
+                value, string = value_parser(string)  # value parsing
+                parsed_list.append(value)
+
+            string = comma_parser(string)
+
+        return (parsed_list, string[1:].strip())
+
+
+def comma_parser(string):
+    if(string[0] != ','):
+        return string.strip()
+    else:
+        return string[1:].strip()
+
+
+def colon_parser(string):
+    if(string[0] != ':'):
+        return None
+    else:
+        return string[1:].strip()
+
 
 def object_parser(string):
+    if(string[0] != '{'):
+        return (None,string.strip())
+
     if(string[0] == '{'):
         parsed_dict = {}
         string = string[1:]
@@ -47,41 +116,33 @@ def object_parser(string):
 
         while(string[0] != '}'):
 
-            if (string[0] == '"'):   # assuming key will be a string
-                key_string_tuple = string_parser(string)
-                key, string = key_string_tuple   # return of string_parser of form ("","") tuple
-                string = string.strip()    # strip after key creation
-            if (string[0] != ':'):
-                raise SyntaxError(" : not found after key")
-            else:
-                string = string[1:]
-                string = string.strip()
+            key, string = string_parser(string)   # return of string_parser of form ("","") tuple
+
+            string = colon_parser(string) # expected : after key
+            if (string is None):
+                raise SyntaxError(": not found")
 
                                           # start of value
-            if (string[0] == '"'):          # if value is a string
-                value_string_tuple = string_parser(string[:])
-                value, string = value_string_tuple
-                string = string.strip()    # strip after value creation
+            value, string = string_parser(string)   # checking for string value
 
-            if (string[0] == '{'):         # if value is a JS object
-                value_dict_tuple = object_parser(string)   #recursive call when value is a dict
-                value, string = value_dict_tuple
+            if (value is None): # if value is a JS object
+                value, string = object_parser(string)
 
-            if (string[0] == '['):
-                value_list_tuple = array_parser(string)
-                value, string = value_list_tuple
+            if (value is None):
+                value, string = array_parser(string)
+                                    # number parser
+            if (value is None):
+                value, string = number_parser(string)
+
+            if (value is None):    # always put value_parser at last
+                value, string = value_parser(string)
 
             string = string.strip()
             parsed_dict[key] = value   # key: value pair generated
 
-
-            if (string[0] == ','):
-                string = string[1:]
-                if(string[0] == '}'):
-                    raise SyntaxError(" no key:value pair found after , ")
+            string = comma_parser(string)
 
             continue
-
 
         return (parsed_dict, string[1:])
 
